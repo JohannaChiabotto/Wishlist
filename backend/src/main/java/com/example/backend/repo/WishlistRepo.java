e74dcf7ae8a38d6ef3999c582396acdba0928d1c
JohannaChiabotto <106194977+JohannaChiabotto@users.noreply.github.com>
2023-01-10 15:57:22 +0100

    Debugging

--- backend/src/main/java/com/example/backend/repo/WishlistRepo.java
+++ backend/src/main/java/com/example/backend/repo/WishlistRepo.java
@@ -2,6 +2,8 @@ package com.example.backend.repo;
 
 import com.example.backend.model.Wishlist;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface WishlistRepo extends MongoRepository<Wishlist,String> {
 }
