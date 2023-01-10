b20cb0c73ab4e97cd6474208fe25c6a684211ff4
JohannaChiabotto <106194977+JohannaChiabotto@users.noreply.github.com>
2023-01-10 14:26:40 +0100

    Add AddWishlist function in WishlistController.java

---
+++ backend/src/main/java/com/example/backend/controller/WishlistController.java
@@ -0,0 +1,28 @@
+package com.example.backend.controller;
+
+import com.example.backend.model.Wishlist;
+import com.example.backend.service.WishlistService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/wishlist")
+public class WishlistController {
+
+    private final WishlistService wishlistService;
+
+    @Autowired
+    public WishlistController(WishlistService wishlistService){
+        this.wishlistService = wishlistService;
+    }
+
+    @GetMapping
+    public Wishlist addWishlist(@RequestBody Wishlist wishlist){
+        return wishlistService.addWishlist(wishlist);
+    }
+}
