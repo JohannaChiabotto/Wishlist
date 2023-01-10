8cf9b9fbcf14336353dbc57a737426499a44fbf4
JohannaChiabotto <106194977+JohannaChiabotto@users.noreply.github.com>
2023-01-10 18:31:54 +0100

    Update endpoint Add annotation

--- backend/src/main/java/com/example/backend/controller/WishlistController.java
+++ backend/src/main/java/com/example/backend/controller/WishlistController.java
@@ -3,10 +3,7 @@ package com.example.backend.controller;
 import com.example.backend.model.Wishlist;
 import com.example.backend.service.WishlistService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,7 +18,7 @@ public class WishlistController {
         this.wishlistService = wishlistService;
     }
 
-    @GetMapping
+    @PostMapping("/create")
     public Wishlist addWishlist(@RequestBody Wishlist wishlist){
         return wishlistService.addWishlist(wishlist);
     }
