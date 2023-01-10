3f34c2f368d81d6179b0d4b9a233feeb2bae20f9
JohannaChiabotto <106194977+JohannaChiabotto@users.noreply.github.com>
2023-01-10 13:20:50 +0100

    Add AddWishlist function backend

--- backend/src/main/java/com/example/backend/model/Wishlist.java
+++ backend/src/main/java/com/example/backend/model/Wishlist.java
@@ -1,8 +1,16 @@
 package com.example.backend.model;
 
+import lombok.With;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
 import java.util.List;
 
+@With
+@Document
 public record Wishlist(
+        @Id
         String wishlistId,
         String name,
         List<Wish>wishes
