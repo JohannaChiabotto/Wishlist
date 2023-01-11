b4d6b80142226d4ea0ab8b835493133c45ee1dcc
JohannaChiabotto <106194977+JohannaChiabotto@users.noreply.github.com>
2023-01-11 10:33:46 +0100

    Add testList

--- backend/src/test/java/com/example/backend/WishlistServiceTest.java
+++ backend/src/test/java/com/example/backend/WishlistServiceTest.java
@@ -6,17 +6,20 @@ import com.example.backend.repo.WishlistRepo;
 import com.example.backend.service.IdService;
 import com.example.backend.service.WishlistService;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.data.domain.Example;
+import org.springframework.http.MediaType;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 import static org.bson.assertions.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
-
-
-
+import static org.springframework.mock.http.server.reactive.MockServerHttpRequest.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 public class WishlistServiceTest {
@@ -24,6 +27,9 @@ public class WishlistServiceTest {
     IdService idService = mock(IdService.class);
     WishlistRepo wishlistRepo = mock(WishlistRepo.class);
     WishlistService wishlistService = new WishlistService(idService, wishlistRepo);
+
+    @InjectMocks
+    private Example example;
 
     List<Wish>wishes = Collections.emptyList();
 
@@ -57,5 +63,30 @@ public class WishlistServiceTest {
             assertNotNull(wishlistService);
         }
 
+
+    @Test
+    public void testList() {
+        Wishlist w1 = new Wishlist("idtest1", "lala", wishes);
+        Wishlist w2 = new Wishlist("idtest2", "lele", wishes);
+        List<Wishlist> expectedWishlists = Arrays.asList(w1, w2);
+        when(wishlistRepo.findAll()).thenReturn(expectedWishlists);
+
+        List<Wishlist> wishlists = wishlistService.list();
+
+        assertEquals(expectedWishlists, wishlists);
+    }
+
+    /*@Test
+    public void testAddWishlistTest() throws Exception {
+        Wishlist wishlist = new Wishlist();
+        wishlist.setName("Test Wishlist");
+        String json = objectMapper.writeValueAsString(wishlist);
+
+        mockMvc.perform(post("/create")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isOk());
+    }
+*/
 
 }
