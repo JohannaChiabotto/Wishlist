b617b220dee6716fef1750d51d8f5017a2a4a89c
JohannaChiabotto <106194977+JohannaChiabotto@users.noreply.github.com>
2023-01-11 12:58:52 +0100

    Delete unused imports

--- backend/src/test/java/com/example/backend/WishlistServiceTest.java
+++ backend/src/test/java/com/example/backend/WishlistServiceTest.java
@@ -7,15 +7,12 @@ import com.example.backend.repo.WishlistRepo;
 import com.example.backend.service.IdService;
 import com.example.backend.service.WishlistService;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.springframework.data.domain.Example;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import static org.bson.assertions.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
@@ -26,8 +23,6 @@ public class WishlistServiceTest {
     WishlistRepo wishlistRepo = mock(WishlistRepo.class);
     WishlistService wishlistService = new WishlistService(idService, wishlistRepo);
 
-    @InjectMocks
-    private Example example;
 
     List<Wish> wishes = Collections.emptyList();
 
@@ -43,22 +38,16 @@ public class WishlistServiceTest {
 
     @Test
     void addWishlistTest() {
-//given
+
         WishlistDTO wishlistRequest = new WishlistDTO("kind1", wishes);
         Wishlist givenWishlist = new Wishlist("123", "kind1", wishes);
-//when
+
         when(idService.generateId()).thenReturn("123");
         when(wishlistRepo.save(givenWishlist)).thenReturn(givenWishlist);
         Wishlist result = wishlistService.addWishlist(wishlistRequest);
-//then
+
         verify(wishlistRepo).save(givenWishlist);
         assertEquals(givenWishlist, result);
-    }
-
-
-    @Test
-    public void testConstructor() {
-        assertNotNull(wishlistService);
     }
 
 
