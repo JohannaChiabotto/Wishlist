0a34c5ad98f6f90d993a0a77a44e85af2bc22e7e
JohannaChiabotto <106194977+JohannaChiabotto@users.noreply.github.com>
2023-01-13 14:55:28 +0100

    Update Integrationtest

--- backend/src/test/java/com/example/backend/WishlistControllerIntegrationTest.java
+++ backend/src/test/java/com/example/backend/WishlistControllerIntegrationTest.java
@@ -4,6 +4,7 @@ import com.example.backend.model.Wishlist;
 import com.example.backend.model.WishlistDTO;
 import com.example.backend.repo.WishlistRepo;
 import com.example.backend.service.IdService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -11,7 +12,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
+import static org.bson.assertions.Assertions.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -27,9 +30,9 @@ class WishlistControllerIntegrationTest {
     @Test
     @DirtiesContext
     void when_wishlist_build() throws Exception {
-        mockMvc.perform(post("/api/wishlist")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content("""
+        MvcResult newWishlistRequestResult = mockMvc.perform(post("/api/wishlist")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
                         {
                              "name": "kind1",
                              "wishes":[{
@@ -38,9 +41,20 @@ class WishlistControllerIntegrationTest {
                                        }]
                         }
                                 """))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String wishlistRequestContent = newWishlistRequestResult.getResponse().getContentAsString();
+        ObjectMapper objectMapper = new ObjectMapper();
+        Wishlist wishlist = objectMapper.readValue(wishlistRequestContent, Wishlist.class);
+
+        assertNotNull(wishlist.wishlistId());
+        assertNotNull(wishlist.wishes().get(0).wishId());
+
 
     }
+
+
 
 
 
