74b24e4aab6f35b1c5e0c53a8a55e5f72309ed62
JohannaChiabotto <106194977+JohannaChiabotto@users.noreply.github.com>
2023-01-16 14:28:20 +0100

    Add first structure for WishlistGallery

--- frontend/src/pages/wishlistGallery/WishlistGalleryPage.tsx
+++ frontend/src/pages/wishlistGallery/WishlistGalleryPage.tsx
@@ -1,6 +1,25 @@
+import {useState} from "react";
+import {Wishlist} from "../../model/Wishlist";
+import WishlistWrapper from "../../components/wishlistWrapper/WishlistWrapper";
+
 export default function WishlistGalleryPage(){
+    const [wishlists, setWishlists] = useState<Array<Wishlist>>([
+        { wishlistId: "id1", name: "Samu", wishes: []},
+        { wishlistId: "id2", name: "Elli", wishes: []},
+        { wishlistId: "id3", name: "Raphaela", wishes: []},
+    ])
+    function getWishlist(){
+       // ...
+    }
+
+    function handleDeleteChange(){
+        getWishlist();
+    }
+
     return( <div>
         <p>See all Wishlists</p>
+
+            {wishlists.map(wishlist => <WishlistWrapper key={wishlist.wishlistId} deleteWishlist={handleDeleteChange} name={wishlist.name} wishlistId={wishlist.wishlistId!} wishes={wishlist.wishes}></WishlistWrapper> )}
         </div>
     )
 
