dde972cae40cf6d528c78adbc6454f88b89c7634
JohannaChiabotto <106194977+JohannaChiabotto@users.noreply.github.com>
2023-01-16 14:26:22 +0100

    Delete unused imports

--- backend/src/test/java/com/example/backend/WishlistControllerIntegrationTest.java
+++ backend/src/test/java/com/example/backend/WishlistControllerIntegrationTest.java
@@ -12,7 +12,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 import static org.bson.assertions.Assertions.assertNotNull;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -48,5 +50,39 @@ class WishlistControllerIntegrationTest {
         assertNotNull(wishlist.wishes().get(0).wishId());
 
 
+    }
+
+    @Test
+    @DirtiesContext
+    void deleteWishlist_shouldDeleteWishlistIfIdExists_whenDeleteRequestIsSuccessful() throws Exception {
+        String saveResult = mockMvc.perform(
+                        post("/api/wishlist")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("""
+                                        {
+                             "name": "kind1",
+                             "wishes":[{
+                                           "name":"wish1",
+                                           "status":"FREE"
+                                       }]
+                        }
+                                        """)
+                )
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Wishlist saveResultWishlist = objectMapper.readValue(saveResult, Wishlist.class);
+        String id = saveResultWishlist.wishlistId();
+
+        mockMvc.perform(delete("/api/wishlist" + id))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(get("/api/wishlist"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                        []
+                        """));
     }
 }
