d5386951c0accb69d5949cb41fb62993786d3cac
JohannaChiabotto <106194977+JohannaChiabotto@users.noreply.github.com>
2023-01-16 21:34:21 +0100

    Add Container

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
-import './App.css';
 import HomePage from "./pages/home/HomePage";
 import NavigationBar from "./components/navigationBar/NavigationBar";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import AddWishlist from "./pages/createWishlist/AddWishlist";
 import WishlistGalleryPage from "./pages/wishlistGallery/WishlistGalleryPage";
 import EditWishlistPage from "./pages/editWishlistPage/EditWishlistPage";
+import Container from "./components/container/Container";
 
 function App() {
-  return (
+    return (
         <BrowserRouter>
             <NavigationBar></NavigationBar>
-            <Routes>
+            <Container>
+                <Routes>
                     <Route path={"/"} element={<HomePage/>}/>
                     <Route path={"/create-wishlist"} element={<AddWishlist/>}/>
                     <Route path={"/wishlist-gallery"} element={<WishlistGalleryPage/>}/>
                     <Route path={"/wishlistid"} element={<EditWishlistPage/>}/>
                     <Route path={"*"} element={<HomePage/>}/>
-            </Routes>
+                </Routes>
+            </Container>
         </BrowserRouter>
-  );
+    );
 }
 
 export default App;
