dbc6149586c9a602cbcc8c4f5efe0800901b320e
JohannaChiabotto <106194977+JohannaChiabotto@users.noreply.github.com>
2023-01-19 22:13:56 +0100

    Refactor style of button and addwishlist component;

--- frontend/src/components/button/Button.tsx
+++ frontend/src/components/button/Button.tsx
@@ -1,23 +1,29 @@
 import style from './Button.module.scss';
 import React, {HTMLAttributes} from "react";
-interface ButtonProps extends HTMLAttributes<HTMLButtonElement>{
+
+interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
     cssClasses?: string;
     onCLickHandler?: () => void;
     type?: 'submit' | 'reset' | 'button';
+    fixed?: boolean;
+    red?: boolean;
 }
-
 
 
 const Button = (props: ButtonProps) => {
 
     function handleClick() {
-        if(props.onCLickHandler){
+        if (props.onCLickHandler) {
             props.onCLickHandler();
         }
     }
 
     return (
-        <button type={props.type} className={style.Button} onClick={handleClick}>
+        <button type={props.type}
+                className={`${style.Button} ${props.red ? style.ButtonRed : ''} ${props.fixed ? style.ButtonFixed  : ''}`}
+                onClick={handleClick}
+        disabled={props.red}>
+
             {props.children}
         </button>
     );
