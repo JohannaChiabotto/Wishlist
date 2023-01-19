1d889cf661ca4514173bcaf9d1bcbbdb71dbe516
JohannaChiabotto <106194977+JohannaChiabotto@users.noreply.github.com>
2023-01-19 22:14:24 +0100

    Refactor style of navigationBar component;

--- frontend/src/components/navigationBar/NavigationBar.tsx
+++ frontend/src/components/navigationBar/NavigationBar.tsx
@@ -1,26 +1,27 @@
 import {NavLink} from "react-router-dom";
 import "./NavigationBar.scss";
 import {GiPresent} from "react-icons/gi";
+import Container from "../container/Container";
 
 
 export default function NavigationBar(){
 
     return (
+        <Container>
         <div className="nav-bar">
             <span className="logo">
-
-
-            <GiPresent className="icon"></GiPresent>
+                <GiPresent className="icon"></GiPresent>
                 Wishlist
-                  </span>
+            </span>
 
             <div className='nav-wrapper'>
             <NavLink to={"/"}>Home</NavLink>
-            <NavLink to={"/create-wishlist"}>Create-Wishlist</NavLink>
-            <NavLink to={"/wishlist-gallery"}>Wishlist-Gallery</NavLink>
+            <NavLink to={"/create-wishlist"}>Add</NavLink>
+            <NavLink to={"/wishlist-gallery"}>Wishlists</NavLink>
             <NavLink to={"/login"} >Login</NavLink>
             <NavLink to={"/logout"} >Logout</NavLink>
             </div>
         </div>
+        </Container>
     )
 }
