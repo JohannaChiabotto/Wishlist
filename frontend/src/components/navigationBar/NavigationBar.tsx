d2f468309899af117408cd4adc2f43879cebe98c
JohannaChiabotto <106194977+JohannaChiabotto@users.noreply.github.com>
2023-01-19 13:15:36 +0100

    Add logo;

--- frontend/src/components/navigationBar/NavigationBar.tsx
+++ frontend/src/components/navigationBar/NavigationBar.tsx
@@ -1,16 +1,26 @@
 import {NavLink} from "react-router-dom";
 import "./NavigationBar.scss";
+import {GiPresent} from "react-icons/gi";
 
 
 export default function NavigationBar(){
 
     return (
         <div className="nav-bar">
+            <span className="logo">
+
+
+            <GiPresent className="icon"></GiPresent>
+                Wishlist
+                  </span>
+
+            <div className='nav-wrapper'>
             <NavLink to={"/"}>Home</NavLink>
             <NavLink to={"/create-wishlist"}>Create-Wishlist</NavLink>
             <NavLink to={"/wishlist-gallery"}>Wishlist-Gallery</NavLink>
             <NavLink to={"/login"} >Login</NavLink>
             <NavLink to={"/logout"} >Logout</NavLink>
+            </div>
         </div>
     )
 }
