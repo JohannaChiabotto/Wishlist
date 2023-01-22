1337c51f57666bca95fc86e4ccaf010f133261fe
JohannaChiabotto <106194977+JohannaChiabotto@users.noreply.github.com>
2023-01-22 16:01:29 +0100

    Add LoginOrRegisterPage

---
+++ frontend/src/pages/loginOrRegister/register/Register.tsx
@@ -0,0 +1,101 @@
+import React, {useState} from "react";
+import style from '../LoginOrRegister.module.scss';
+import Button from "../../../components/button/Button";
+import Input from "../../../components/input/Input";
+
+type RegisterProps = {
+    handleTypeOfUser: (arg: boolean) => void
+}
+
+export default function Register(props: RegisterProps) {
+    const [register, setRegister] = useState({
+        username: '',
+        email: '',
+        password: '',
+        samePassword: '',
+    });
+
+    function handleRegisterChange(event: React.FormEvent<HTMLInputElement>) {
+        const value = event.currentTarget.value;
+        const id = event.currentTarget.id;
+
+        switch (id) {
+            case 'username':
+                setRegister(prevState => {
+                    return {...prevState, username: value}
+                });
+                break;
+            case 'email':
+                setRegister(prevState => {
+                    return {...prevState, email: value}
+                });
+                break;
+
+            case 'password':
+                setRegister(prevState => {
+                    return {...prevState, password: value}
+                });
+                break;
+            case 'samePassword':
+                setRegister(prevState => {
+                    return {...prevState, samePassword: value}
+                });
+                break;
+        }
+
+    }
+
+    function handleSubmitLoginChange(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+
+
+        /*
+       axios.post("/api/...", wishlistRequest)
+            .then()
+            .catch(console.error)
+            */
+    }
+
+    function redirectToLogin() {
+        props.handleTypeOfUser(true);
+    }
+
+
+    return (
+        <div><h2 className={style.Subtitle}>Register</h2>
+
+            <div className={style.Wrapper}>
+                <div className={style.WrapperForm}>
+                    <form onSubmit={handleSubmitLoginChange}>
+                        <div className={style.InputWrapper}>
+                            <Input id={'username'} label={'your username:'} value={register.username}
+                                   changeWishHandler={handleRegisterChange}></Input>
+                        </div>
+                        <div className={style.InputWrapper}>
+                            <Input id={'email'} label={'your Email:'} value={register.email}
+                                   changeWishHandler={handleRegisterChange}></Input>
+                        </div>
+                        <div className={style.InputWrapper}>
+                            <Input id={'password'} label={'your password:'} value={register.password}
+                                   changeWishHandler={handleRegisterChange}></Input>
+                        </div>
+                        <div className={style.InputWrapper}>
+                            <Input id={'samePassword'} label={'Repeat your password:'} value={register.samePassword}
+                                   changeWishHandler={handleRegisterChange}></Input>
+                        </div>
+                        <div className={style.ButtonWrapper}>
+
+                            <Button type="submit">save</Button>
+                        </div>
+                    </form>
+                </div>
+                <div className={style.WrapperButton}>
+                    <p>Do you already have an username and password?</p>
+                    <Button type="button" onCLickHandler={redirectToLogin}>go to login</Button></div>
+            </div>
+        </div>
+
+
+    );
+}
+
