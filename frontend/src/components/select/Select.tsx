f8cb57baddada0bce15002bd3e5e9dad0ed63c76
JohannaChiabotto <106194977+JohannaChiabotto@users.noreply.github.com>
2023-01-22 16:02:42 +0100

    Refactor several ui Components

--- frontend/src/components/select/Select.tsx
+++ frontend/src/components/select/Select.tsx
@@ -1,29 +1,24 @@
-import React, {ChangeEventHandler, HTMLAttributes} from "react";
+import React, {ChangeEventHandler} from "react";
 import {WishStatus} from "../../model/WishStatus";
 import style from './Select.module.scss'
 
-interface SelectProps extends HTMLAttributes<HTMLInputElement> {
+interface SelectProps {
     status: WishStatus,
-    changeStatusHandler: ChangeEventHandler<HTMLInputElement>;
+    id: string,
+    changeStatusHandler: ChangeEventHandler<HTMLSelectElement>;
 }
 
 const Select = (props: SelectProps) => {
 
     return (
-        <select name="status"  className={style.Select}>
-            <option selected={props.status === WishStatus.FREE}
-                    value="FREE"> free
-            </option>
-            <option
-                selected={props.status === WishStatus.BOUGHT}
-                value="BOUGHT"> bought
-            </option>
-            <option
-                selected={props.status === WishStatus.RESERVE}
-                value="RESERVE"> reserved
-            </option>
-        </select>
-
+        <>
+            <select value={props.status} onChange={props.changeStatusHandler} name="status" id={`select-${props.id}
+            `} className={style.Select}>
+                <option value="FREE">free</option>
+                <option value="BOUGHT">bought</option>
+                <option value="RESERVE">reserved</option>
+            </select>
+        </>
     )
 };
 
