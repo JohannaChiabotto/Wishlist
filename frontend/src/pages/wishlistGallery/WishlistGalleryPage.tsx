a492388a5d909d962f65370d81c446917f7d9f93
JohannaChiabotto <106194977+JohannaChiabotto@users.noreply.github.com>
2023-01-23 23:51:24 +0100

    Add fallback text if the wishlist array is empty

--- frontend/src/pages/wishlistGallery/WishlistGalleryPage.tsx
+++ frontend/src/pages/wishlistGallery/WishlistGalleryPage.tsx
@@ -22,7 +22,10 @@ export default function WishlistGalleryPage() {
 
     return (<>
             <h1>See all Wishlists</h1>
-            {wishlists.map(wishlist =>
+
+            {wishlists.length === 0 ? <Card>
+                <p>Hey, there are still no wishlist here.<br/>Go and 'Add' and create your first one!</p>
+            </Card> : wishlists.map(wishlist =>
                 <Card key={wishlist.wishlistId}>
                     <WishlistWrapper key={wishlist.wishlistId}
                                      deleteWishlist={handleDeleteChange}
