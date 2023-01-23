5f2b9598c7fd5e08cfc12ec43ecb6899e8fc3152
JohannaChiabotto <106194977+JohannaChiabotto@users.noreply.github.com>
2023-01-23 16:13:50 +0100

    Update home text

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -10,9 +10,10 @@ export default function Home() {
         <Container>
             <h1>Welcome back, Username</h1>
             <Card>
-                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
-                    labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-                    et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+                <p>Hallo liebe Freunde und Familie!
+                wie schön, dass ihr eine Liste für uns erstellen wollt, um uns das schenken noch schöner machen zu können!</p>
+                <p>Ich freue mich schon sehr auf die nächsten Geburtstage und Weihnachten, um diese tolle App  benutzen zu können!</p>
+                <p>PS: hier werden keine Tiere, verschenkt, es sei denn, man kann sie essen ;)</p>
                 <div className={style.ButtonWrapper}>
                     <Link to={"/create-wishlist"}>
                         <Button>Create your own wishlist</Button>
