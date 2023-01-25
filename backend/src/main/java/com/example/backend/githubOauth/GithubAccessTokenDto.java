7c78f44dda2b79fbb263fbd2e01d7d92bb9d93f2
JohannaChiabotto <106194977+JohannaChiabotto@users.noreply.github.com>
2023-01-25 13:09:35 +0100

    Delete oauth github

--- backend/src/main/java/com/example/backend/githubOauth/GithubAccessTokenDto.java
+++
@@ -1,9 +0,0 @@
-package com.example.backend.githubOauth;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public record GithubAccessTokenDto(
-        @JsonProperty("access_token")
-        String accessToken
-) {
-}
