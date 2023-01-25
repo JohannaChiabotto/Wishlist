783743f3c05c24c2a0553c6b877df0e97f791610
JohannaChiabotto <106194977+JohannaChiabotto@users.noreply.github.com>
2023-01-25 12:15:46 +0100

    Update Endpoint

--- backend/src/main/java/com/example/backend/controller/WishlistController.java
+++ backend/src/main/java/com/example/backend/controller/WishlistController.java
@@ -24,7 +24,7 @@ public class WishlistController {
         return wishlistService.createWishlist(wishlistRequest);
     }
 
-    @GetMapping("{id}")
+    @GetMapping("/wishlists/{id}")
     public Wishlist getWishlist(@PathVariable String id) throws IllegalAccessException {
         return wishlistService.findWishlistById(id);
     }
