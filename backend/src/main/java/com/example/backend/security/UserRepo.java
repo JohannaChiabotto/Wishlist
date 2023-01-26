440085750c37728a3cb165e3c114e853a868db05
JohannaChiabotto <106194977+JohannaChiabotto@users.noreply.github.com>
2023-01-26 13:34:04 +0100

    Update userrepo backend

---
+++ backend/src/main/java/com/example/backend/security/UserRepo.java
@@ -0,0 +1,12 @@
+package com.example.backend.security;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepo extends MongoRepository<User,String> {
+    Optional<User> findByName(String name);
+}
+
