f3a3f76c02adccf15e5b65916c42f286b1ff26b5
JohannaChiabotto <106194977+JohannaChiabotto@users.noreply.github.com>
2023-01-26 14:36:05 +0100

    Update useUser

--- frontend/src/hooks/useUser.tsx
+++ frontend/src/hooks/useUser.tsx
@@ -1,9 +1,25 @@
 import {useEffect, useState} from "react";
 import axios from "axios";
+import {Wishlist} from "../model/Wishlist";
+import {User} from "../model/User";
 
 export default function useUser(){
 
-    const [userName, setUserName] = useState<string>("")
+    const emptyWishlist: Wishlist={
+        "wishlistId": "",
+        "name": "",
+        "wishes": []
+    }
+
+    const emptyUser: User={
+        "id":"",
+        "username":"",
+        "password": "",
+        "email": "",
+        "wishlist": [emptyWishlist]
+    }
+
+    const [userName, setUserName] = useState<User>(emptyUser)
 
     useEffect(()=>{
         axios.get("/api/users/me")
